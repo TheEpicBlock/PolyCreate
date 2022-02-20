740950bbd61959df97f323d281cc26e90245c7d1
TheEpicBlock <git.teb@theepicblock.nl>
2022-02-20 19:48:08 +0100

    Kinda implement crushing wheels

--- src/main/java/nl/theepicblock/polycreate/PolyCreate.java
+++ src/main/java/nl/theepicblock/polycreate/PolyCreate.java
@@ -4,7 +4,10 @@ import com.simibubi.create.AllBlocks;
 import com.simibubi.create.AllEntityTypes;
 import io.github.theepicblock.polymc.api.PolyMcEntrypoint;
 import io.github.theepicblock.polymc.api.PolyRegistry;
+import io.github.theepicblock.polymc.impl.poly.block.SimpleReplacementPoly;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import nl.theepicblock.polycreate.blocks.KineticBlockPoly;
 import nl.theepicblock.polycreate.entity.PotatoProjectileWizard;
 
@@ -19,6 +22,13 @@ public class PolyCreate implements ModInitializer, PolyMcEntrypoint {
 		registry.registerBlockPoly(AllBlocks.SHAFT.get(), new KineticBlockPoly(registry));
 		registry.registerBlockPoly(AllBlocks.COGWHEEL.get(), new KineticBlockPoly(registry));
 		registry.registerBlockPoly(AllBlocks.LARGE_COGWHEEL.get(), new KineticBlockPoly(registry));
+		registry.registerBlockPoly(AllBlocks.CRUSHING_WHEEL.get(), new KineticBlockPoly(registry) {
+			@Override
+			public BlockState getClientBlock(BlockState input) {
+				return Blocks.BARRIER.getDefaultState();
+			}
+		});
+		registry.registerBlockPoly(AllBlocks.CRUSHING_WHEEL_CONTROLLER.get(), new SimpleReplacementPoly(Blocks.SWEET_BERRY_BUSH)); // FIXME, berry bushes behave differently from crushing wheels
 
 		registry.registerEntityPoly(AllEntityTypes.POTATO_PROJECTILE.get(), PotatoProjectileWizard::new);
 	}
