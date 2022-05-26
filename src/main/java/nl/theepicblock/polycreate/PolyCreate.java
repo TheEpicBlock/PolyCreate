eb732b7d50059096492fc0ea5a38bbab7fcb331f
TheEpicBlock <git.teb@theepicblock.nl>
2022-05-26 14:58:50 +0200

    Remove regular entrypoint

--- src/main/java/nl/theepicblock/polycreate/PolyCreate.java
+++ src/main/java/nl/theepicblock/polycreate/PolyCreate.java
@@ -9,18 +9,13 @@ import io.github.theepicblock.polymc.api.PolyMcEntrypoint;
 import io.github.theepicblock.polymc.api.PolyRegistry;
 import io.github.theepicblock.polymc.api.entity.EntityPoly;
 import io.github.theepicblock.polymc.impl.poly.block.SimpleReplacementPoly;
-import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import nl.theepicblock.polycreate.blocks.KineticBlockPoly;
 import nl.theepicblock.polycreate.entity.ContraptionPoly;
 import nl.theepicblock.polycreate.entity.PotatoProjectileWizard;
 
-public class PolyCreate implements ModInitializer, PolyMcEntrypoint {
-
-	@Override
-	public void onInitialize() {
-	}
+public class PolyCreate implements PolyMcEntrypoint {
 
 	@Override
 	public void registerPolys(PolyRegistry registry) {
